b9c51a62e659e24080af5479c0667a91f33d328b
vishal rokade <vrokade2013@gmail.com>
2022-03-12 14:15:15 +0530

    first commit

---
+++ Array.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Array
+{
+ public static void main(String args[])
+  {
+
+    int []arr = new int[5];
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter"+arr.length+"numbers:");
+                    
+ 
+    for(int i=0;i<arr.length;i++)
+     {
+        arr[i]= sc.nextInt();
+     }
+      print_array(arr);
+   }
+
+static void print_array(int []brr)
+  {
+  for(int i=0;i<brr.length;i++)
+   {
+     System.out.println("brr["+i+"]:"+brr[i]);
+   }
+  }
+}   
